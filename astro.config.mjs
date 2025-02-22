6ae14f8ced3708b505213f4c8c23f1268e179017
dqcuong <dqcuong19@gmail.com>
2025-02-23 00:10:13 +0700

    Clean code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,13 @@ import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
 import { SITE } from "./src/lib/config";
 import { modifiedTime, readingTime } from "./src/lib/utils/remarks.mjs";
-
 import sitemap from "@astrojs/sitemap";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
-  site: SITE.website || process.env.SITE_WEBSITE,
+  site: SITE.url || process.env.SITE_WEBSITE,
   base: SITE.basePath,
   output: "server",
   adapter: node({ mode: "standalone" }),
