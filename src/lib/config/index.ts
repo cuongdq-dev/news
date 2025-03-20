43d66116b30efe97f4d7b67151f3a781290d700c
dqcuong <dqcuong19@gmail.com>
2025-03-20 17:06:18 +0700

    update meta tags

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -4,9 +4,10 @@ export const SITE = {
   title: import.meta.env.SITE_TITLE || process.env.SITE_TITLE,
   description: import.meta.env.SITE_DESC || process.env.SITE_DESC,
   author: import.meta.env.SITE_AUTHOR || process.env.SITE_AUTHOR,
+  keywords: import.meta.env.SITE_KEYWORDS || process.env.SITE_KEYWORDS,
   profile: import.meta.env.SITE_PROFILE || process.env.SITE_PROFILE,
   url: import.meta.env.SITE_WEBSITE || process.env.SITE_WEBSITE,
-  ogImage: "default-og.jpg",
+  ogImage: "favicon/news_logo_512.png",
 
   locale: "en-US",
   dir: "ltr",
